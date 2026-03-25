d5b5d08725dcd03cb5ba30840ee12b059595adc8
VietHoang <viethoangcbvn0301@gmail.com>
2026-03-25 20:33:56 +0700

    Feat: Map API

--- app/src/main/java/com/example/btl_hethongdatsancaulong/controllers/MapActivity.java
+++ app/src/main/java/com/example/btl_hethongdatsancaulong/controllers/MapActivity.java
@@ -1,11 +1,25 @@
 package com.example.btl_hethongdatsancaulong.controllers;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+// Nhớ import mấy thư viện của Google Map này nhé
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.example.btl_hethongdatsancaulong.R;
 import com.example.btl_hethongdatsancaulong.databinding.ActivityMapBinding;
 
-public class MapActivity extends AppCompatActivity {
+// Thêm cái implements OnMapReadyCallback vào đuôi class
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
     private ActivityMapBinding binding;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +27,33 @@ public class MapActivity extends AppCompatActivity {
         binding = ActivityMapBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Khởi động bản đồ ngầm
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
         // Nút Back
         binding.btnMapLayer.setOnClickListener(v -> finish());
+    }
+
+    // Hàm này tự động chạy khi bản đồ đã load xong
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // 1. Tạo tọa độ (Kinh độ, Vĩ độ) cho 3 sân bóng ở Hà Nội
+        LatLng sanSuPham = new LatLng(21.0365, 105.7818);
+        LatLng sanXuanThuy = new LatLng(21.0375, 105.7828);
+        LatLng sanMyDinh = new LatLng(21.0223, 105.7628);
+
+        // 2. Cắm cờ (Marker) lên bản đồ
+        mMap.addMarker(new MarkerOptions().position(sanSuPham).title("Sân Cầu Lông ĐHSP"));
+        mMap.addMarker(new MarkerOptions().position(sanXuanThuy).title("Sân Cầu Lông Xuân Thủy"));
+        mMap.addMarker(new MarkerOptions().position(sanMyDinh).title("Sân Cầu Lông Mỹ Đình"));
+
+        // 3. Di chuyển Camera (Góc nhìn) về khu vực Cầu Giấy và zoom lên (mức 14f)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sanSuPham, 14f));
     }
 }
