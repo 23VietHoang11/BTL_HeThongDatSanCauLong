89ae7850937afbc103a571aa2df3c8a543ed2488
VietHoang <viethoangcbvn0301@gmail.com>
2026-03-26 14:15:50 +0700

    Feat: Thanh Toán

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Thư viện Glide để load ảnh QR từ Internet
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
